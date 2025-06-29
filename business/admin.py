05da1aa1c0688427c70077004912be62eb3c9e2d
Ayush Shah <shahayush001@gmail.com>
2025-06-30 01:42:23 +0530

    Remove business ratings and reviews

--- business/admin.py
+++ business/admin.py
@@ -7,21 +7,10 @@ class BusinessAdmin(admin.ModelAdmin):
      search_fields = ('name', 'vendor', 'id')
      list_filter = ('area', 'city', 'category')
 
-class BusinessRatingAdmin(admin.ModelAdmin):
-     list_display = ('id', 'user', 'shop', 'rating')
-     list_filter = ('rating', 'user', 'shop')
-
-class BusinessReviewAdmin(admin.ModelAdmin):
-     list_display = ('id', 'user', 'shop', 'review')
-     search_fields = ('id', 'review')
-     list_filter = ('shop', 'user')
-
 class BusinessCategoryAdmin(admin.ModelAdmin):
      list_display = ('id', 'name')
      search_fields = ('id', 'name')
 
 
 admin.site.register(Business, BusinessAdmin)
-admin.site.register(BusinessRating, BusinessRatingAdmin)
-admin.site.register(BusinessReview, BusinessReviewAdmin)
 admin.site.register(BusinessCategory, BusinessCategoryAdmin)
